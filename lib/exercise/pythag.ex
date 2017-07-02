e90e46fe55ee40c9c8a3ee6ffd325ed489055deb
Raymond Maung <raymaung@gmail.com>
2017-07-02 23:37:04 +1000

    Add timer

--- lib/exercise/pythag.ex
+++ lib/exercise/pythag.ex
@@ -1,13 +1,16 @@
 defmodule Exercise.Pythag do
 
-  def run() do
-    limit = 100
+  def run(limit \\ 100) do
     for a <- 1..limit,
         b <- 1..limit,
         c <- 1..limit,
         (a * a) + (b * b) == (c * c) do
       {a, b, c}
     end
+  end
+
+  def run_with_timer(limit \\ 100) do
+    :timer.tc(fn -> run(limit) end) |> IO.inspect
   end
 
 end
