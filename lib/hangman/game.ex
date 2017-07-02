072b967ca21fc24671e1ef9fdb9373f3ff9ed74f
Raymond Maung <raymaung@gmail.com>
2017-07-02 23:04:40 +1000

    Handle duplicate moves

--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -3,7 +3,8 @@ defmodule Hangman.Game do
   defstruct(
     turns_left: 7,
     game_state: :initializing,
-    letters: []
+    letters: [],
+    used: MapSet.new()
   )
 
   def new_game() do
@@ -14,6 +15,19 @@ defmodule Hangman.Game do
 
   def make_move(game = %{game_state: state}, _guess) when state in [:won, :lost] do
     {game, tally(game)}
+  end
+
+  def make_move(game, guess) do
+    game = accept_move(game, guess, MapSet.member?(game.used, guess))
+    {game, tally(game)}
+  end
+
+  defp accept_move(game, guess, _already_guessed = true) do
+    Map.put(game, :game_state, :already_used)
+  end
+
+  defp accept_move(game, guess, _already_guessed) do
+    Map.put(game, :used, MapSet.put(game.used, guess))
   end
 
   defp tally(game) do
