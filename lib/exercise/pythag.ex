77db0e1aa02d44c4acb2016d547c828d7c35375f
Raymond Maung <raymaung@gmail.com>
2017-07-11 21:57:40 +1000

    Optimize pythag

--- lib/exercise/pythag.ex
+++ lib/exercise/pythag.ex
@@ -2,8 +2,8 @@ defmodule Exercise.Pythag do
 
   def run(limit \\ 100) do
     for a <- 1..limit,
-        b <- 1..limit,
-        c <- 1..limit,
+        b <- (a+1)..limit,
+        c <- (b+1)..limit,
         (a * a) + (b * b) == (c * c) do
       {a, b, c}
     end
